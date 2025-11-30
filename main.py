143dd948cbd618e01c211b305463e70e4ca8c183
laurenhquan <laurenhquan@gmail.com>
2025-11-30 01:23:05 -0800

    set up pygame

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pygame
+
+# initialize game
+pygame.init()
+
+# set up game window
+screen = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("Block Blast Buddy")
+
+# loop game
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type ==pygame.QUIT:
+            running = False
+
+# quit game
+pygame.quit()
